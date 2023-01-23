6467a30d073bae6748b5d71f12f82e28585bdff4
LeninC <cordovalenin.44@gmail.com>
2023-01-23 17:41:55 -0500

    Crear paquetes

--- src/MVC_Main/Main.java
+++ src/MVC_Main/Main.java
@@ -2,19 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package bdguia2;
+package MVC_Main;
 
 /**
  *
  * @author Lenin
  */
-public class BDGUIA2 {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //
+        // TODO code application logic here
     }
     
 }
