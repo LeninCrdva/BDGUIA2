3da0c1012a9c0500e926d8273d7ebbf85f4a4c93
Santiago G. <santiagogcan@gmail.com>
2023-02-01 00:13:58 -0500

    ControladorTipoCamion completo

--- src/Controlador/ControladorTipoCam.java
+++ src/Controlador/ControladorTipoCam.java
@@ -3,9 +3,9 @@ package Controlador;
 import Modelo.*;
 import Vista.*;
 import java.awt.event.KeyAdapter;
-import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class ControladorTipoCam {
@@ -20,9 +20,12 @@ public class ControladorTipoCam {
     }
     
     public void IniciaControl() {
+        CargaTipos();
         vtp.getBtnlistartip().addActionListener(l -> CargaTipos());
         vtp.getBtncreartip().addActionListener(l -> AbrirDialogo(1));
         vtp.getBtneditartip().addActionListener(l -> AbrirDialogo(2));
+        vtp.getBtneditartip().addActionListener(l -> mtp.ObtieneIdConsulta(vtp.getTxtnombretip().getText()));
+        vtp.getBtneditartip().addActionListener(l -> nana());
         vtp.getBtneliminartip().addActionListener(l -> AbrirDialogo(3));
         vtp.getBtnaceptartip().addActionListener(l -> CrearEditarEliminarTipoCam());
         vtp.getBtncanceltip().addActionListener(l -> vtp.getDlgCrudTip().setVisible(false));
@@ -32,11 +35,43 @@ public class ControladorTipoCam {
                 BuscaTipoCamion();
             }
         });
-        
+        vtp.getTxtnombretip().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                ValidaCampotxt(evt);
+            }
+        });
     }
     
+    public void nana(){
+        System.out.println(vtp.getTxtnombretip().getText());
+    }
+    
+    public void ValidaCampotxt(java.awt.event.KeyEvent e) {
+        char key = e.getKeyChar();
+        if (Character.isDigit(key) || vtp.getTxtnombretip().getText().length() >= 50) {
+            e.consume();
+        }
+    }
+    
+    private boolean MousePress(JTable me) {
+        boolean press = false;
+        try { 
+            if (me.getSelectedRowCount() == 1) {
+                vtp.getTxtnombretip().setText(vtp.getTbltipocam().getValueAt(vtp.getTbltipocam().getSelectedRow(), 1).toString());
+                press = true;
+            } else {
+                JOptionPane.showMessageDialog(vtp, "Seleccione una fila primero");
+            }
+        } catch(NullPointerException e) {
+            System.out.println(e);
+        }
+        return press;
+    }
+        
     public void AbrirDialogo(int ce) {
-        String title;
+        String title = null;
+        boolean iscorrect = false;
         switch(ce) {
             case 1:
                 title = "CREAR TIPO CAMIÓN";
@@ -44,26 +79,38 @@ public class ControladorTipoCam {
                 vtp.getDlgCrudTip().setTitle(title);
                 vtp.getTxtnombretip().setText(null);
                 vtp.getTxtnombretip().setEnabled(true);
+                vtp.getLblvalidanom().setText(null);
+                iscorrect = true;
                 break;
             case 2:
                 title = "MODIFICAR";
                 vtp.getDlgCrudTip().setName("editar");
                 vtp.getDlgCrudTip().setTitle(title);
-                vtp.getTxtnombretip().setEnabled(true);
+                vtp.getTxtnombretip().setEnabled(false);
+                vtp.getLblvalidanom().setText(null);
+                iscorrect = MousePress(vtp.getTbltipocam());
                 break;
             case 3:
                 title = "ELIMINAR";
                 vtp.getDlgCrudTip().setName("eliminar");
                 vtp.getDlgCrudTip().setTitle(title);
                 vtp.getTxtnombretip().setEnabled(false);
+                vtp.getLblvalidanom().setText(null);
+                iscorrect = MousePress(vtp.getTbltipocam());
                 break;
             default:
                 break;
         }
+        if (iscorrect) {
+            vtp.getDlgCrudTip().setLocationRelativeTo(vtp);
+            vtp.getDlgCrudTip().setTitle(title);
+            vtp.getDlgCrudTip().setSize(400, 200);
+            vtp.getDlgCrudTip().setVisible(true);
+        }
     }
     
     public void CargaTipos() {
-        List<Tipo_camion> listatip = new ArrayList<>();
+        List<Tipo_camion> listatip = mtp.listarTipos();
         DefaultTableModel mtable;
         mtable = (DefaultTableModel) vtp.getTbltipocam().getModel();
         mtable.setNumRows(0);
@@ -95,20 +142,33 @@ public class ControladorTipoCam {
         });
     }
     
+    
+    private boolean validaCampo(String nombre){
+        return nombre.isEmpty();
+    }
+    
     private void CrearEditarEliminarTipoCam() {
         if (vtp.getDlgCrudTip().getName().equals("crear")) {
             try {
                 int id_tip = IncrementaID();
-                String nombre = vtp.getTxtnombretip().getText();
-                
-                ModeloTipoCam tip = new ModeloTipoCam();
-                tip.setId_tip(id_tip);
-                tip.setNombre_tipo(nombre);
-                if (tip.InsertarTipoCamion() == null) {
-                    vtp.getDlgCrudTip().setVisible(false);
-                    JOptionPane.showMessageDialog(vtp, "Tipo creado correctamente");
+                String nombre = vtp.getTxtnombretip().getText().toUpperCase().trim();
+                if (mtp.ExisteNombreTipo(nombre)) {
+                    vtp.getLblvalidanom().setText("Ya Registrado");
                 } else {
-                    JOptionPane.showMessageDialog(vtp, "No se pudo crear el tipo");
+                    if (validaCampo(nombre)) {
+                        vtp.getLblvalidanom().setText("Vacío");
+                    } else {
+                        ModeloTipoCam tip = new ModeloTipoCam();
+                        tip.setId_tip(id_tip);
+                        tip.setNombre_tipo(nombre);
+                        vtp.getLblvalidanom().setText(null);
+                        if (tip.InsertarTipoCamion() == null) {
+                            vtp.getDlgCrudTip().setVisible(false);
+                            JOptionPane.showMessageDialog(vtp, "Tipo creado correctamente");
+                        } else {
+                            JOptionPane.showMessageDialog(vtp, "No se pudo crear el tipo");
+                        }
+                    }
                 }
             } catch(NullPointerException | NumberFormatException e) {
                 System.out.println(e);
@@ -116,19 +176,25 @@ public class ControladorTipoCam {
         } else {
             if (vtp.getDlgCrudTip().getName().equals("editar")) {
                 try {
-                    String nombre = vtp.getTxtnombretip().getText();
+                    String nombre = vtp.getTxtnombretip().getText().toUpperCase().trim();
+                    int id_tip = mtp.ObtieneIdConsulta(nombre);
                     if (nombre.isEmpty()) {
                             JOptionPane.showMessageDialog(vtp, "Por favor seleccione una fila de la tabla");
                     } else {
                         vtp.getBtnaceptartip().setEnabled(true);
-                        ModeloTipoCam tip = new ModeloTipoCam();
-
-                            tip.setNombre_tipo(vtp.getTxtnombretip().getText());
-                        if (tip.ModficarTipoCamion(nombre) == null) {
-                            vtp.getDlgCrudTip().setVisible(false);
-                            JOptionPane.showMessageDialog(vtp, "Tipo editado con éxito");
+                        if (mtp.ExisteNombreTipo(nombre)) {
+                            vtp.getLblvalidanom().setText("Ya Registrado");
                         } else {
-                            JOptionPane.showMessageDialog(vtp, "No se pudo editar el tipo");
+                            ModeloTipoCam tip = new ModeloTipoCam();
+                            tip.setId_tip(id_tip);
+                            tip.setNombre_tipo(nombre);
+                            vtp.getLblvalidanom().setText(null);
+                            if (tip.ModficarTipoCamion(id_tip) == null) {
+                                vtp.getDlgCrudTip().setVisible(false);
+                                JOptionPane.showMessageDialog(vtp, "Tipo editado con éxito");
+                            } else {
+                                JOptionPane.showMessageDialog(vtp, "No se pudo editar el tipo");
+                            }
                         }
                     }
                 } catch(NumberFormatException | NullPointerException e) {
@@ -137,18 +203,24 @@ public class ControladorTipoCam {
             } else {
                 if (vtp.getDlgCrudTip().getName().equals("eliminar")) {
                     try {
-                        String nombre = vtp.getTxtnombretip().getText();
+                        String nombre = vtp.getTxtnombretip().getText().toUpperCase();
+                        int id = mtp.ObtieneIdConsulta(nombre);
                         if (nombre.isEmpty()) {
                             JOptionPane.showMessageDialog(vtp, "Por favor seleccione una fila de la tabla");
                         } else {
-                            ModeloTipoCam tip = new ModeloTipoCam();
-
-                            tip.setNombre_tipo(vtp.getTxtnombretip().getText());
-                            if (tip.EliminarTipoCamion(nombre) == null) {
-                                vtp.getDlgCrudTip().setVisible(false);
-                                JOptionPane.showMessageDialog(vtp, "Tipo eliminado con éxito");
+                            if (mtp.ExisteAsociacion(nombre)) {
+                                vtp.getLblvalidanom().setText("Registro asociado");
+                                JOptionPane.showMessageDialog(vtp, "Imposible eliminar, está asociado a uno o más registros");
                             } else {
-                                JOptionPane.showMessageDialog(vtp, "No se pudo eliminar el tipo");
+                                vtp.getLblvalidanom().setText(null);
+                                ModeloTipoCam tip = new ModeloTipoCam();
+                                tip.setNombre_tipo(vtp.getTxtnombretip().getText());
+                                if (tip.EliminarTipoCamion(id) == null) {
+                                    vtp.getDlgCrudTip().setVisible(false);
+                                    JOptionPane.showMessageDialog(vtp, "Tipo eliminado con éxito");
+                                } else {
+                                    JOptionPane.showMessageDialog(vtp, "No se pudo eliminar el tipo");
+                                }
                             }
                         }
                     } catch(NumberFormatException | NullPointerException e) {
